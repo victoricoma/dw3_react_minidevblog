592ae3813001d1f54361dd0e605c2341cd52504f
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-04-02 12:34:12 -0300

    Aula0204 Styles e Props

--- basic/basictools/src/App.jsx
+++ basic/basictools/src/App.jsx
@@ -4,7 +4,21 @@ import Footer from './components/Footer/Footer'
 import Form from './components/Form/Form'
 import FormHooks from './components/Hooks/FormHooks'
 import ConditionalRender from './components/ConditionalRender/ConditionalRender'
+import ShowUserName from './components/ShowUserName/ShowUserName'
+import { useState } from 'react'
 function App() {
+  const [users, setUser] = useState([
+    {active: true, userName:'Icoma'},
+    {active: true, userName:'Sena'},
+    {active: false, userName:'Hammer'},
+    {active: true, userName:'Salomao'},
+    {active: true, userName:'Hammer'},
+    {active: true, userName:'Salomao'},
+    {active: true, userName:'Hammer'},
+    {active: true, userName:'Salomao'},
+    {active: true, userName:'Hammer'},
+    {active: true, userName:'Salomao'}
+  ])
   return (
     <>
     <NavBar></NavBar>
@@ -12,6 +26,7 @@ function App() {
     <Footer></Footer>
     <FormHooks></FormHooks>
     <ConditionalRender></ConditionalRender>
+    <ShowUserName list={users}></ShowUserName>
     </>
   )
 }
