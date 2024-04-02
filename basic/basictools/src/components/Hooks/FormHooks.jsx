90e8517fc3e72602f1a8f964cb7afa8d2c9e7ebd
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-04-02 10:28:46 -0300

    Aula2603 e 0204 Renderizadores com Hooks

--- basic/basictools/src/components/Hooks/FormHooks.jsx
+++ basic/basictools/src/components/Hooks/FormHooks.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const FormHooks = () => {
     const [list] = useState(['Letícia', 'Enzo', 'Kayro', 'Gustavo']) 
     const [number, setNumber] = useState(15)
-    const [user] = useState(
+    const [user, setUser] = useState(
         [
             {id:1, nome: 'José Carlos', idade: 44},
             {id:2, nome: 'Maria Rosa', idade: 25},
@@ -15,6 +15,13 @@ const FormHooks = () => {
             {id:8, nome: 'Pedro Lucas', idade: 37}
         ]
     )
+    const deleteRandonUser =()=>{
+        const randomNumber = Math.floor(Math.random() * user.length)
+        setUser((prevUsers) => {
+            console.log(prevUsers);
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+    }
     return (
         <>
             <div>
@@ -55,6 +62,7 @@ const FormHooks = () => {
                         ))
                     }
                 </table>
+                <button onClick={deleteRandonUser}>Excluir Randomicamente</button>
             </div>
         </>
     )
