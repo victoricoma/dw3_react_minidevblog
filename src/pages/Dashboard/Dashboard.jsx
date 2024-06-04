baf1d398d8e5f99c8a5618148b0ba5d57d33638c
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-06-04 12:09:16 -0300

    Aula0406 Hooks de Firebase terminados

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Dashboard.module.css"
+import { Link } from 'react-router-dom'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
